5a519ed9b44203f83eddb035e9b704b433072b63
individual-L <ljh15074125070@163.com>
2024-03-02 15:18:58 +0800

    three day_03

---
+++ sample_server/build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../server"
+  "../server.pdb"
+  "CMakeFiles/server.dir/sample_ser.cpp.o"
+  "CMakeFiles/server.dir/sample_ser.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
